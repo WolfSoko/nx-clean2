4a10d4751eb17f71ad1947537d01ec20844aece7
Wolfram <wolframsokollek@gmail.com>
2024-02-24 19:14:41 +0100

    ci: 👷 vercel use dynamic import

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
-const server = require('../dist/apps/nx-clean2/server/server.mjs');
+async function loadServer() {
+  const serverModule = await import('../dist/apps/nx-clean2/server/server.mjs');
+  return serverModule.app;
+}
 
-export default server.app();
+export default loadServer().then((app) => app());
