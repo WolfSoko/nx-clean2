dae11029b1a1b52dd5163a75e510d22257f236ab
Wolfram <wolframsokollek@gmail.com>
2024-02-24 19:56:45 +0100

    feat: ✨ add library to have a better example
    - simplified lint staged tp prettier

---
+++ libs/shared/src/lib/utils/utils.component.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UtilsComponent } from '@clean-nx/utils';
+
+describe('UtilsComponent', () => {
+  let component: UtilsComponent;
+  let fixture: ComponentFixture<UtilsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UtilsComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UtilsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render text', () => {
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('p').textContent).toContain(
+      'Shared utils component is working!'
+    );
+  });
+});
