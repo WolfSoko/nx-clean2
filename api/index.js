2266d9ac20047ece5adda293bf5c0980ebe4cea6
Wolfram <wolframsokollek@gmail.com>
2024-02-24 16:40:33 +0100

    ci: 👷 vercel try js instead of mjs

---
+++ api/index.js
@@ -0,0 +1,3 @@
+import { app } from '../dist/apps/nx-clean2/server/server.mjs';
+
+export default app;
--- api/index.mjs
+++
@@ -1,3 +0,0 @@
-import * as server from '../dist/apps/nx-clean2/server/server.mjs';
-
-export default server.app;
